d4e48b59710d7af34e6cb93255ea6e2c28dfb962
Gaston Volpellier <gaston.volpellier@gmail.com>
2023-09-04 10:47:09 +0200

    feat: added refresh functionality for coupons & profile.

--- src/screens/Coupons.tsx
+++ src/screens/Coupons.tsx
@@ -1,5 +1,12 @@
-import React, {useState} from 'react';
-import {View, Text, ScrollView, Pressable, SafeAreaView} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  ScrollView,
+  Pressable,
+  SafeAreaView,
+  RefreshControl,
+} from 'react-native';
 import styles from '../styles/styles';
 import fonts from '../styles/fonts';
 import componentStyles from '../styles/components';
@@ -7,7 +14,9 @@ import {backgroundColors, colors, fontColors} from '../styles/variables';
 import Header from '../components/Header';
 import ImageSectionAlt from '../components/ImageSectionAlt';
 import CouponModal from '../components/CouponModal';
-import {useData} from '../utils/DataProvider';
+import * as api from '../services/api';
+import {useSession} from '../utils/SessionProvider';
+import Spinner from '../components/Spinner';
 
 export default function Coupons({navigation}: Props): JSX.Element {
   const [modalVisible, setModalVisible] = useState(false);
@@ -16,16 +25,48 @@ export default function Coupons({navigation}: Props): JSX.Element {
     number: '',
     expiration: '',
     status: 0,
+    url: '',
   });
   const [filter, setFilter] = useState(0);
-  const {couponsList} = useData();
+  const [isLoading, setIsLoading] = useState(true);
+  const [couponsList, setCouponsList] = useState();
+  const {idToken} = useSession();
 
-  const handleModalData = (title, number, expiration, status) => {
+  const onRefresh = React.useCallback(() => {
+    setIsLoading(true);
+    getCoupons();
+  }, []);
+
+  const getCoupons = async () => {
+    try {
+      const data = await api.fetchHistory(idToken, 0);
+
+      console.log('Cupones fetched ', data.cupones);
+
+      if (data?.error == false) {
+        const couponsLength = Object.keys(data.cupones).length;
+        couponsLength > 0 ? setCouponsList(data.cupones) : null;
+
+        setIsLoading(false);
+      } else {
+        console.log('Data error: ', data);
+      }
+    } catch (error) {
+      console.log('Error fetching coupons: ', error);
+    }
+  };
+
+  useEffect(() => {
+    getCoupons();
+  }, []);
+
+  const handleModalData = (title, number, expiration, status, url) => {
     setModalData({
       title: title,
       number: number,
       expiration: expiration,
       status: status,
+      url: url,
     });
     setModalVisible(!modalVisible);
   };
@@ -42,7 +83,10 @@ export default function Coupons({navigation}: Props): JSX.Element {
         modalData={modalData}
       />
       <ScrollView
-        style={[componentStyles.mainContainer, backgroundColors.quaternary]}>
+        style={[componentStyles.mainContainer, backgroundColors.quaternary]}
+        refreshControl={
+          <RefreshControl refreshing={isLoading} onRefresh={onRefresh} />
+        }>
         <Text
           style={[
             fonts.secondaryMain,
@@ -62,6 +106,22 @@ export default function Coupons({navigation}: Props): JSX.Element {
           ]}>
           <ScrollView style={[{flexDirection: 'row'}, styles.mb20]} horizontal>
             <Pressable
+              onPress={() => setFilter(3)}
+              style={[
+                componentStyles.carouselButton,
+                filter === 3
+                  ? backgroundColors.primary
+                  : backgroundColors.secondary,
+              ]}>
+              <Text
+                style={[
+                  fonts.primarySmall,
+                  filter === 3 ? fontColors.secondary : fontColors.primary,
+                ]}>
+                Todos
+              </Text>
+            </Pressable>
+            <Pressable
               onPress={() => setFilter(0)}
               style={[
                 componentStyles.carouselButton,
@@ -74,7 +134,7 @@ export default function Coupons({navigation}: Props): JSX.Element {
                   fonts.primarySmall,
                   filter === 0 ? fontColors.secondary : fontColors.primary,
                 ]}>
-                Todos
+                Activos
               </Text>
             </Pressable>
             <Pressable
@@ -90,48 +150,54 @@ export default function Coupons({navigation}: Props): JSX.Element {
                   fonts.primarySmall,
                   filter === 1 ? fontColors.secondary : fontColors.primary,
                 ]}>
-                Activos
-              </Text>
-            </Pressable>
-            <Pressable
-              onPress={() => setFilter(2)}
-              style={[
-                componentStyles.carouselButton,
-                filter === 2
-                  ? backgroundColors.primary
-                  : backgroundColors.secondary,
-              ]}>
-              <Text
-                style={[
-                  fonts.primarySmall,
-                  filter === 2 ? fontColors.secondary : fontColors.primary,
-                ]}>
                 Inactivos
               </Text>
             </Pressable>
           </ScrollView>
-          {couponsList.map(coupon =>
-            filter == 0 || filter == coupon.status ? (
-              <Pressable
-                key={coupon.id}
-                onPress={() => {
-                  handleModalData(
-                    coupon.title,
-                    coupon.number,
-                    coupon.expiration,
-                    coupon.status,
-                  );
-                }}
-                style={styles.mb20}>
-                <ImageSectionAlt
-                  image={coupon.image}
-                  altDescription={coupon.altDescription}
-                  title={coupon.title}
-                  description={coupon.description}
-                  status={coupon.status}
-                />
-              </Pressable>
-            ) : null,
+          {isLoading ? (
+            <View style={{paddingTop: 50, minHeight: 300}}>
+              <Spinner />
+            </View>
+          ) : (
+            <View style={{minHeight: 300}}>
+              {couponsList ? (
+                couponsList.map(coupon =>
+                  filter == 3 || filter == coupon.estado ? (
+                    <Pressable
+                      key={coupon.id}
+                      onPress={() => {
+                        handleModalData(
+                          coupon.promocion.titulo,
+                          coupon.codigo,
+                          coupon.fecha_fin,
+                          coupon.estado,
+                          coupon.cupon_qr,
+                        );
+                      }}
+                      style={styles.mb20}>
+                      <ImageSectionAlt
+                        imageURL={coupon.promocion.foto}
+                        altDescription={coupon.promocion.extracto}
+                        title={coupon.promocion.titulo}
+                        description={coupon.promocion.validez}
+                        status={coupon.estado}
+                      />
+                    </Pressable>
+                  ) : null,
+                )
+              ) : (
+                <Text
+                  style={[
+                    fonts.primary,
+                    styles.horizontalPadding,
+                    styles.homePadding,
+                    fontColors.primary,
+                    styles.textAlignC,
+                  ]}>
+                  Aun no tiene Cupones en su perfil.
+                </Text>
+              )}
+            </View>
           )}
         </View>
       </ScrollView>
